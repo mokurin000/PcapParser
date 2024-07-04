170e3f47d7df1f6b9cb985b4ccae2fe71fd01418
poly000 <1348292515a@gmail.com>
2024-07-04 21:16:50 +0800

    initial commit

---
+++ src/pcapparser/__main__.py
@@ -0,0 +1,29 @@
+import re
+from tkinter.filedialog import askopenfilename
+
+import dpkt
+
+pcap_file = askopenfilename(defaultextension="pcap", filetypes=[("Pcap", ".pcap")])
+urls = set()
+
+with open(pcap_file, "rb") as f:
+    pcap = dpkt.pcap.Reader(f)
+    rtmp_url_re = b"rtmp://.*/live"
+    token_re = rb"\d+\?token=[\w\d]+&t=\d+"
+
+    base = None
+    for t, buf in pcap:
+        if b"rtmp://" in buf:
+            r = re.search(rtmp_url_re, buf)
+            base = f"{r.group().decode("ascii")}"
+        if b"FCSubscribe" in buf:
+            r = re.search(token_re, buf)
+            if r is not None and base is not None:
+                auth = r.group().decode("ascii")
+                url = f"{base}/{auth}"
+                urls.add(url)
+                base = None
+
+result = "\n".join(urls)
+with open("urls.txt", "w", encoding="utf-8") as f:
+    f.write(result)
